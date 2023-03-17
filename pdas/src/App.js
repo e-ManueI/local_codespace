4c60de1827dc6e3e5c9472d5508725f42502998c
e-ManueI <19648538+e-ManueI@users.noreply.github.com>
2023-03-17 21:21:36 +0300

    34. Including Images

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,9 +1,19 @@
 import ProfileCard from "./ProfileCard";
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
+console.log(AlexaImage);
+console.log(SiriImage);
 function App() { 
     return (
         <div>
             <div>Personal Digital Assistants</div>
+
+            <img src={AlexaImage} alt="Alexa logo"/>
+            <img src={CortanaImage} alt="Cortana logo" />
+            <img src={SiriImage} alt="Siri logo" />
+
             <ProfileCard title="Alexa" handle="@alexa99" />
             <ProfileCard title="Cortana" handle="@cortana32" />
             <ProfileCard title="Siri" handle="@siri01" />

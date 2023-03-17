7977e544f544e064c639dd76a3a075eeca280879
e-ManueI <19648538+e-ManueI@users.noreply.github.com>
2023-03-17 23:48:31 +0300

    38. A Big Pile of HTML!

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -9,10 +9,22 @@ function App() {
     return (
         <div>
             <h1>Personal Digital Assistants</h1>
- 
-            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
-            <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
-            <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+
+            <div className='container'>
+                <section className='section'>
+                    <div className='columns'>
+                        <div className='column is-4'>
+                            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+                        </div>
+                    </div>
+                </section>
+            </div>  
         </div>
     );
 }

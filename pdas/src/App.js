f4963b7377f17de5cc8c6aade4ea4defefe1e63a
e-ManueI <19648538+e-ManueI@users.noreply.github.com>
2023-03-17 21:31:40 +0300

    35. Handling Image Accessibility

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -3,20 +3,15 @@ import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
 import SiriImage from './images/siri.png';
 
-console.log(AlexaImage);
-console.log(SiriImage);
+
 function App() { 
     return (
         <div>
-            <div>Personal Digital Assistants</div>
-
-            <img src={AlexaImage} alt="Alexa logo"/>
-            <img src={CortanaImage} alt="Cortana logo" />
-            <img src={SiriImage} alt="Siri logo" />
-
-            <ProfileCard title="Alexa" handle="@alexa99" />
-            <ProfileCard title="Cortana" handle="@cortana32" />
-            <ProfileCard title="Siri" handle="@siri01" />
+            <h1>Personal Digital Assistants</h1>
+ 
+            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+            <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+            <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
         </div>
     );
 }

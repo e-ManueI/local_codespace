c5381ddccce837d4bf3737f64ea70c479500d9ca
e-ManueI <19648538+e-ManueI@users.noreply.github.com>
2023-03-18 10:19:53 +0300

    43. Variations on Event Handlers

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,10 +1,15 @@
 function App() {
-    const handleClick = () => {
-        console.log('Button was clicked!')
-    };
-    return <div>
-        <button onClick={handleClick}>Add Animal</button>
-    </div>
+    // const handleClick = () => {
+    //     console.log('Button was clicked!')
+    // };
+    return (
+        <div>
+            <button onClick={ () =>
+                console.log('Button was clicked')} >
+                Add Animal
+            </button>
+        </div>
+    )
 }
 
 export default App;

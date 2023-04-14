a37427b211ef188090c72e2bb4fa7180c9799321
e-ManueI <19648538+e-ManueI@users.noreply.github.com>
2023-04-15 01:11:03 +0300

    46. Introducing the State System

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,15 +1,22 @@
+import { useState } from 'react';
+
 function App() {
     // const handleClick = () => {
     //     console.log('Button was clicked!')
     // };
+
+    const [count, SetCount] = useState(0);
+
+    const handleClick = () => {
+        SetCount(count + 1);
+    };
+
     return (
         <div>
-            <button onClick={ () =>
-                console.log('Button was clicked')} >
-                Add Animal
-            </button>
+            <button onClick={handleClick} >Add Animal</button>
+            <div>Number of animals: {count}</div>
         </div>
-    )
+    );
 }
 
 export default App;

2313ff51d8803e35f38ac03d773dbb8c3439b669
e-ManueI <19648538+e-ManueI@users.noreply.github.com>
2023-04-15 18:05:45 +0300

    66. Data fetching cleanup

--- pics/src/api.js
+++ pics/src/api.js
@@ -1,18 +1,16 @@
 import axios from 'axios';
 
-const searchImages = async () => {
+const searchImages = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
             Authorization: 'Client-ID Ogd7NEvrK0s9pqJHxdVdjzbY8hdn1YOz02V1J23MNwQ'
         },
         params: {
-            query: 'cars'
-        }
+            query: term,
+        },
     });
 
-    console.log(response);
-
-    return response;
+    return response.data.results;
 };
 
 export default searchImages;

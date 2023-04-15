7deab53276b19c8920e6220ebb77f3449a74c760
e-ManueI <19648538+e-ManueI@users.noreply.github.com>
2023-04-15 08:26:16 +0300

    49. Why Array Destructuring

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,22 +1,29 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
 function App() {
     // const handleClick = () => {
     //     console.log('Button was clicked!')
     // };
+    function makeArray() {
+        return [1, 10, 32, 40];
+    }
+    const [firstElement, secondElement] = makeArray();
+    console.log(firstElement, secondElement);
+    // const myArray = makeArray()
+    // const firstElement = myArray[0]
+    // const secondElement = myArray[1]
+    // const [count, SetCount] = useState(0);
 
-    const [count, SetCount] = useState(0);
+    // const handleClick = () => {
+    //     SetCount(count + 1);
+    // };
 
-    const handleClick = () => {
-        SetCount(count + 1);
-    };
-
-    return (
-        <div>
-            <button onClick={handleClick} >Add Animal</button>
-            <div>Number of animals: {count}</div>
-        </div>
-    );
+    // return (
+    //     <div>
+    //         <button onClick={handleClick} >Add Animal</button>
+    //         <div>Number of animals: {count}</div>
+    //     </div>
+    // );
 }
 
 export default App;
